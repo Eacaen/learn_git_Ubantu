b42ff29bf93d29ad3f9ff738dac324fa881acb9f
Eacaen <501556184@qq.com>
2017-09-14 23:12:55 +0800

    add latex / abaqus

---
+++ del_tex.sh
@@ -0,0 +1,6 @@
+find .  -name "*.log" -exec rm -rf {} \;
+find .  -name "*.aux" -exec rm -rf {} \;
+find .  -name "*.gz" -exec rm -rf {} \;
+find .  -name "*.aux" -exec rm -rf {} \;
+find .  -name "*.toc" -exec rm -rf {} \;
+find .  -name "*.pyc" -exec rm -rf {} \;
